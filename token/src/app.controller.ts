e9ab10fbe2f23007fdce87ff3749102a41662825
Ricardo Menotti <86802680+menottiRicardo@users.noreply.github.com>
2022-09-15 12:07:36 -0500

    acknoledge ready

--- token/src/app.controller.ts
+++ token/src/app.controller.ts
@@ -1,16 +1,28 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { JwtPayload } from 'jsonwebtoken';
-import { Ctx, MessagePattern, Payload, RmqContext } from '@nestjs/microservices';
+import {
+  Ctx,
+  MessagePattern,
+  Payload,
+  RmqContext,
+} from '@nestjs/microservices';
 import { IDecodeResponse } from './core/interfaces/IDecodeResponse';
 import { ITokenResponse } from './core/interfaces/ITokenResponse';
+import { RmqService } from './rmq/rmq.service';
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly rmqService: RmqService,
+  ) {}
 
   @MessagePattern('test')
-  public async createToken(@Payload() data: any, @Ctx() context: RmqContext): Promise<any> {
-    return this.appService.createToken(data.test);
+  public async createToken(
+    @Payload() data: any,
+    @Ctx() context: RmqContext,
+  ): Promise<any> {
+    this.appService.createToken(data.test);
+    return this.rmqService.ack(context);
   }
-
 }
